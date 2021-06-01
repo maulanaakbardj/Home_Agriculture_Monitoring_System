4780b09e98fde8b8401402778052d27dc14ca1dd
dewasanta2 <dewasanta2@gmail.com>
2021-06-01 15:14:31 +0800

    add main-code

---
+++ electronics/main-code/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <DHT.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Adafruit_Sensor.h>
+
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
