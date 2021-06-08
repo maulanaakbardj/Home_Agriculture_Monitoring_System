e2793fa267b0ec42cf6a37dea102c4b29d75fdeb
dewasanta2 <dewasanta2@gmail.com>
2021-06-08 23:30:41 +0800

    edit main code, now the device can be connected to cloud and apps

--- electronics/main-code/lib/server-lib/serverlib.cpp
+++ electronics/main-code/lib/server-lib/serverlib.cpp
@@ -1,76 +1,77 @@
-#include <serverlib.h>
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+#include "serverlib.h"
+#include <Arduino.h>
 
-const char *server_ssid = "test";
-const char *server_pass = "123456789";
+const char *ssid = "MyESP32AP";
+const char *password = "testpassword";
 
 const char *PARAM_INPUT_1 = "input1";
 const char *PARAM_INPUT_2 = "input2";
 const char *PARAM_INPUT_3 = "input3";
+const char *PARAM_INPUT_4 = "input4";
 
-void initWiFi()
-{
-    WiFi.softAP(server_ssid, server_pass);
-    Serial.print("Creating WiFi ..");
-    //   while (WiFi.status() != WL_CONNECTED)
-    //   {
-    //     Serial.print('.');
-    //     delay(1000);
-    //   }
-    Serial.println("kontol");
-    Serial.println(WiFi.softAPIP());
-}
-
-void notFound(AsyncWebServerRequest *request)
-{
-    request->send(404, "text/plain", "Not found");
-}
+const char server_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>ESP Input Form</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head><body>
+  <form action="/get">
+    input1: <input type="text" name="input1">
+    input2: <input type="text" name="input2">
+    input3: <input type="text" name="input3">
+    input4: <input type="text" name="input4">
+    <input type="submit" value="Submit">
+  </form>
+</body></html>)rawliteral";
 
 void make_server()
 {
-    Serial.begin(9600);
-    Serial.println("mulai");
-    // Create AsyncWebServer object on port 80
-    AsyncWebServer server(80);
+  AsyncWebServer server(80);
+  WiFi.softAP(ssid, password);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
 
-    // Create an Event Source on /events
-    AsyncEventSource events("/events");
-
-    initWiFi();
-
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-              { request->send_P(200, "text/html", server_html); });
-
-    // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
-    server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send_P(200, "text/html", server_html); });
+  // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
+  server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              String inputMessage_1 = "";
+              String inputMessage_2 = "";
+              String inputMessage_3 = "";
+              String inputMessage_4 = "";
+              String inputParam;
+              // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+              if (request->hasParam(PARAM_INPUT_1))
               {
-                  String inputMessage;
-                  String inputParam;
-                  // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
-                  if (request->hasParam(PARAM_INPUT_1))
-                  {
-                      inputMessage = request->getParam(PARAM_INPUT_1)->value();
-                      inputParam = PARAM_INPUT_1;
-                  }
-                  // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
-                  else if (request->hasParam(PARAM_INPUT_2))
-                  {
-                      inputMessage = request->getParam(PARAM_INPUT_2)->value();
-                      inputParam = PARAM_INPUT_2;
-                  }
-                  // GET input3 value on <ESP_IP>/get?input3=<inputMessage>
-                  else if (request->hasParam(PARAM_INPUT_3))
-                  {
-                      inputMessage = request->getParam(PARAM_INPUT_3)->value();
-                      inputParam = PARAM_INPUT_3;
-                  }
-                  else
-                  {
-                      inputMessage = "No message sent";
-                      inputParam = "none";
-                  }
-                  Serial.println(inputMessage);
-                  request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" + inputParam + ") with value: " + inputMessage + "<br><a href=\"/\">Return to Home Page</a>");
-              });
-    server.onNotFound(notFound);
-    server.begin();
+                inputMessage_1 = request->getParam(PARAM_INPUT_1)->value();
+                inputParam = PARAM_INPUT_1;
+              }
+              // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
+              if (request->hasParam(PARAM_INPUT_2))
+              {
+                inputMessage_2 = request->getParam(PARAM_INPUT_2)->value();
+                inputParam = PARAM_INPUT_2;
+              }
+              // GET input3 value on <ESP_IP>/get?input3=<inputMessage>
+              if (request->hasParam(PARAM_INPUT_3))
+              {
+                inputMessage_3 = request->getParam(PARAM_INPUT_3)->value();
+                inputParam = PARAM_INPUT_3;
+              }
+              if (request->hasParam(PARAM_INPUT_4))
+              {
+                inputMessage_4 = request->getParam(PARAM_INPUT_4)->value();
+                inputParam = PARAM_INPUT_4;
+              }
+              else
+              {
+                inputMessage_1 = "No message sent";
+                inputParam = "none";
+              }
+              Serial.println(inputMessage_1);
+              request->send(200, "text/html", "HTTP GET : " + inputMessage_1 + ", " + inputMessage_2 + ", " + inputMessage_3 + ", " + inputMessage_4);
+            });
+  server.begin();
 }
